a38d497de3b307e0b5d4eef3709b9afc0c9c6644
Harry Jang <harry.jang@kakao.com>
2023-08-26 08:28:13 +0900

    1차 완성

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,10 @@
 import propTypes from "prop-types";
 import {Link} from "react-router-dom"
 
-function Movie ({coverImage, title, summry, genres}) {
+function Movie ({id, coverImage, title, summry, genres}) {
     return <div>
     <img src={coverImage} alt={title} />
-    <h2><Link to="/movie">{title}</Link></h2>
+    <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
     <p>{summry}</p>
     <ul>
       {genres && genres.map((genre) => <li key={genre}>{genre}</li>)}
@@ -13,6 +13,7 @@ function Movie ({coverImage, title, summry, genres}) {
 }
 
 Movie.propTypes = {
+    id : propTypes.number.isRequired,
     coverImage : propTypes.string.isRequired,
     title: propTypes.string.isRequired,
     summary: propTypes.string.isRequired,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -16,14 +16,15 @@ function Home() {
   useEffect(() => {
     getMovies();
   }, []);
-  
+
   return (
     <div>
       {loading ? <h1>Loading...</h1> : (
       <div>
         {movies.map(movie => (
           <Movie
-            key={movie.id} 
+            key={movie.id}
+            id={movie.id} 
             coverImage={movie.medium_cover_image}
             title={movie.title} 
             summary={movie.summary} 
