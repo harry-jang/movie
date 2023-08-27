231f3abc5d27963d07b4720c3f7d1c889a58005e
Harry Jang <harry.jang@kakao.com>
2023-08-27 21:34:39 +0900

    github 페이지 배포 설정 적용

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Detail from "./routes/Detail";
 
 import { createBrowserRouter} from "react-router-dom"
 
-
+console.log(`${process.env.PUBLIC_URL}`)
 const App = createBrowserRouter([
   { 
     path: "/",
     element:<Home />,
   },
   {
-    path: "detail/:id",
+    path: "/detail/:id",
     element:<Detail />,
   }
-]);
+], { basename:`${process.env.PUBLIC_URL}`});
 
 export default App;
 
